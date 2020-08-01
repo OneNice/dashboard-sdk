3563c727b0e74b5902dc5452334c1c6e6bbe2de7
gennadijbakunovic <ionenice@yandedx.ru>
2020-08-01 20:47:50 +0300

    Init

---
+++ src/DashboardServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace OZiTAG\Tager\Backend\Admin;
+
+use Illuminate\Foundation\Support\Providers\EventServiceProvider;
+use Illuminate\Support\Facades\Route;
+use OZiTAG\Tager\Backend\Admin\Observers\TokenObserver;
+
+class DashboardServiceProvider extends EventServiceProvider
+{
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        $this->loadRoutesFrom(__DIR__ . '/../routes/routes.php');
+    }
+}
